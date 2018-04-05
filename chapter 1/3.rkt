04a96edbec2356c226f2dfc9ef2095fe2a6baef5
Patrick Olila <polila@ryerson.ca>
2018-04-05 12:50:55 -0400

    Chapter 1 Question 3

---
+++ chapter 1/3.rkt
@@ -0,0 +1,12 @@
+#lang racket
+
+
+(define (square a) (* a a))
+
+(define (f a b c)
+  (define (max-iter x y z sum count)
+    (cond ((= count 2) sum)
+          ((> x y) (max-iter z y z (+ sum (square x)) (+ count 1)))
+          (else (max-iter x z z (+ sum (square y)) (+ count 1)))))
+  (max-iter a b c 0 0))
+         
